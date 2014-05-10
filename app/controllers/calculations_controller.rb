11dc22c248c2d1d00b5d81110f4187228e3a66c8
drewbgenter <drewbgenter@gmail.com>
2014-05-10 11:37:45 -0500

    integers

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -5,11 +5,15 @@ class CalculationsController < ApplicationController
   def sq
     @the_number = params[:number].to_f
     @answer = (@the_number)*(@the_number)
+    @answeri = @answer.to_i
+    @the_numberi = @the_number.to_i
   end
 
   def sqrt
     @the_number = params[:number].to_f
     @answer = Math.sqrt(@the_number)
+    @answeri = @answer.to_i
+    @the_numberi = @the_number.to_i
   end
 
   def pmt
@@ -17,6 +21,7 @@ class CalculationsController < ApplicationController
     @nper = params[:number_of_payments].to_f
     @pval = params[:present_value].to_f
     @payment = @pval * (@rate * ((1 + @rate) ** @nper) / ((1 + @rate) ** @nper - 1))
+    @answer = @payment.to_i
   end
 end
 
